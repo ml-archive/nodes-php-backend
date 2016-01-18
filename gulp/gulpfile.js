4cd0c046c8629033b3686e033587bd75a2cebf16
Dennis Haulund Nielsen <dhni@nodes.dk>
2016-01-18 16:22:53 +0100

    Update gulpfile and gulp config

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -6,42 +6,51 @@
  *
  */
 var elixir = require('laravel-elixir');
+var gulp = require('gulp');
 
-var config					= require('./vendor/nodesagency/backend/gulp/config.json');
+var config					= require('./gulp/config.json');
 var pkg						= require('./package.json');
 
 var projectName 			= pkg.name;
 var projectUrl				= 'https://' + pkg.name + '.local-like.st';
 
 var filesToWatch = [
-    './public/**/*',
-    './resources/views/**/*',
-    './resources/assets/**/*',
-    './vendor/nodesagency/backend/resources/views/**/*'
+	'./public/**/*',
+	'./resources/views/**/*',
+	'./vendor/nodesagency/backend/resources/views/**/*',
+	'./nodes/backend/resources/views/**/*',
 ];
-
-require(config.tasksPath + 'vendor-scripts.task.js');
-require(config.tasksPath + 'backend-scripts.task.js');
-require(config.tasksPath + 'backend-styles.task.js');
-require(config.tasksPath + 'project-scripts.task.js');
-require(config.tasksPath + 'project-styles.task.js');
-require(config.tasksPath + 'modernizr.task.js');
 
 elixir.config.js.outputFolder = 'public/js';
 elixir.config.css.outputFolder = 'public/css';
 
+require(config.tasksPath + 'vendor-scripts.task.js');
+
+require(config.tasksPath + 'project-scripts.task.js');
+require(config.tasksPath + 'project-styles.task.js');
+require(config.tasksPath + 'project-scss-wiredep.task.js');
+
 elixir(function(mix) {
 
 	mix.vendorScripts();
-	mix.backendScripts();
-	mix.backendStyles();
+
+	mix.projectScssWiredep();
 	mix.projectStyles();
 	mix.projectScripts();
-	mix.modernizr();
 
-	mix.browserSync(filesToWatch, {
+	mix.browserSync({
+		files: filesToWatch,
 		proxy: projectUrl,
 		https: true
 	});
 
 });
+
+gulp.task('build', [
+	'project-scss-wiredep',
+	'vendor-scripts',
+	'project-scripts',
+	'project-pages-scripts',
+	'project-partials-scripts',
+	'project-styles'
+]);

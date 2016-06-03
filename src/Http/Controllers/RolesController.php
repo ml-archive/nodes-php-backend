1d7d4b32c13ab7b20870da01ea16f49f2b70ec90
Casper Rasmussen <cr@nodes.dk>
2016-06-03 16:54:54 +0200

    Update RolesController.php

--- src/Http/Controllers/RolesController.php
+++ src/Http/Controllers/RolesController.php
@@ -161,7 +161,7 @@ class RolesController extends Controller
     public function setDefault($id) {
         // Retrieve role by ID
         $role = $this->roleRepository->getById($id);
-        if (!empty($role)) {
+        if (empty($role)) {
             return redirect()->route('nodes.backend.users.roles')->with('error', 'Role does not exist');
         }
 

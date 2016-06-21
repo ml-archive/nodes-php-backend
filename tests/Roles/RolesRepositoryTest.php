4377a4c371361f4d85c5ade764168b27d887981b
Casper Rasmussen <cr@nodes.dk>
2016-06-21 13:20:18 +0000

    trying testbench package

--- tests/Roles/RolesRepositoryTest.php
+++ tests/Roles/RolesRepositoryTest.php
@@ -3,9 +3,18 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Nodes\Backend\ServiceProvider;
 
-class RolesRepositoryTest extends \TestCase
+class RolesRepositoryTest extends Orchestra\Testbench\TestCase
 {
+    protected function getPackageProviders($app)
+    {
+        return [
+            ServiceProvider::class,
+            \Nodes\Backend\Auth\ServiceProvider::class
+        ];
+    }
+
     /**
      * A basic functional test example.
      *

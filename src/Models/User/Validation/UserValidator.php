79eca985cf1015a085946b53912268cc8653adbb
Casper Rasmussen <cr@nodes.dk>
2017-03-21 08:05:57 +0000

    min 8 chars

--- src/Models/User/Validation/UserValidator.php
+++ src/Models/User/Validation/UserValidator.php
@@ -20,7 +20,7 @@ class UserValidator extends AbstractValidator
             'email' => ['required', 'email', 'unique:backend_users,email,{:id}', 'max:190'],
             'password' => [
                 'required_without:id',
-                'min:6',
+                'min:8',
                 'confirmed',
                 'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\X])(?=.*[!$#%]).*$/'
             ],
@@ -30,7 +30,7 @@ class UserValidator extends AbstractValidator
         'update-password' => [
             'password' => [
                 'required_without:id',
-                'min:6',
+                'min:8',
                 'confirmed',
                 'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\X])(?=.*[!$#%]).*$/'
             ],
